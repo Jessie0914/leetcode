e037b4182fcebbb5d97a80e89e041fef42258abd
Jessie0914 <894673968@qq.com>
2019-11-12 13:34:04 +0800

    更新

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @ClassName Test
  * @Description TODO
@@ -6,7 +9,13 @@
  **/
 public class Test {
     public static void main(String[] args) {
-        int i=12;
-        System.out.println(i+=i-=i*=i);
+        String s = "abcwaquigew";
+        Map<Character, Integer> hashMap = new HashMap<>();
+        for (int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            int count = hashMap.getOrDefault(c, 0);
+            count+=1;
+            hashMap.put(c,count);
+        }
     }
 }
