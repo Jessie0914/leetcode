aa8bd03609dfaafa5d5e40af8eee0745bc72461c
Jessie0914 <894673968@qq.com>
2019-11-27 14:25:19 +0800

    提交

---
+++ src/test/java/Solution756Test.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution756Test {
+
+    @Test
+    public void pyramidTransition() {
+        Solution756 solution756 = new Solution756();
+        List<String> allowed = new ArrayList<>();
+        allowed.add("XXX");
+        allowed.add("XXY");
+        allowed.add("XYX");
+        allowed.add("XYY");
+        allowed.add("YXZ");
+        boolean res = solution756.pyramidTransition("XXYX", allowed);
+        System.out.println(res);
+    }
+}
