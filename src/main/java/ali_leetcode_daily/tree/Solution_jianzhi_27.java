1afa676c5fb05fd8acf127dc57755e7033a32b06
chiyue.sjx <chiyue.sjx@alibaba-inc.com>
2020-06-28 11:59:20 +0800

    6月28号提交-01

---
+++ src/main/java/ali_leetcode_daily/tree/Solution_jianzhi_27.java
@@ -0,0 +1,16 @@
+package ali_leetcode_daily.tree;
+
+/**
+ * @author chiyue
+ * @Description 剑指 Offer 27. 二叉树的镜像
+ * @date 2020/6/28 11:58 上午
+ */
+
+/**
+ * 请完成一个函数，输入一个二叉树，该函数输出它的镜像。
+ */
+public class Solution_jianzhi_27 {
+    public TreeNode mirrorTree(TreeNode root) {
+
+    }
+}
