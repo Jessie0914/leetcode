42fb057aa2513c3d5a1054ef7393963dcb899911
shijiaxin <894673968@qq.com>
2020-06-30 23:21:58 +0800

    2020年6月30号更新-1

--- src/main/java/ali_leetcode_daily/dynamic_programming/Solution322.java
+++ src/main/java/ali_leetcode_daily/dynamic_programming/Solution322.java
@@ -6,5 +6,32 @@ package ali_leetcode_daily.dynamic_programming;
  * @Author shishi
  * @Date 2020/6/30 20:41
  **/
+
+import java.util.Arrays;
+
+/**
+ * 输入: coins = [1, 2, 5], amount = 11
+ * 输出: 3
+ * 解释: 11 = 5 + 5 + 1
+ */
 public class Solution322 {
+    public int coinChange(int[] coins, int amount) {
+        if (amount < 0 || coins == null || coins.length == 0)
+            return 0;
+
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+
+        // base case
+        dp[0] = 0;
+
+        for (int i = 1; i < amount + 1; i++) {
+            for (int coin : coins) {
+                if (i - coin < 0)
+                    continue;
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+            }
+        }
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
+    }
 }
