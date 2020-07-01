abfd128463b6f0b77d8087e02f24d7dc43439127
chiyue.sjx <chiyue.sjx@alibaba-inc.com>
2020-07-01 15:10:19 +0800

    07月01号提交-01

---
+++ src/main/java/ali_leetcode_daily/dynamic_programming/Solution416.java
@@ -0,0 +1,77 @@
+package ali_leetcode_daily.dynamic_programming;
+
+/**
+ * 416. 分割等和子集
+ */
+public class Solution416 {
+    // 二维
+    public boolean canPartition(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum % 2 != 0)
+            return false;
+        sum /= 2;
+
+        int len = nums.length;
+        boolean[][] dp = new boolean[len + 1][sum + 1];
+
+        // base case
+        dp[0][0] = true;
+        for (int i = 1; i < len + 1; i++) {
+            dp[i][0] = true; // 代表前i个元素，都不选，就能凑出0
+        }
+        for (int j = 1; j < sum + 1; j++) {
+            dp[0][j] = false;
+        }
+
+        // dp
+        for (int i = 1; i < len + 1; i++) {
+            for (int j = 1; j < sum + 1; j++) {
+                // 如果装的下第i-1个元素
+                // 可以选择装或者不装
+                if (j - nums[i - 1] >= 0)
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+                    // 如果装不下，那么只能选择不装
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+
+        return dp[len][sum];
+    }
+
+    // 可以压缩到一维的
+    public boolean canPartition2(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum % 2 != 0)
+            return false;
+        sum /= 2;
+
+        boolean[] dp = new boolean[sum + 1];
+        dp[0] = true;
+        for (int i = 0; i < nums.length; i++) {
+            // 内层循环，j逆序！！！
+            // 否则，如果正序的话，前面的数据先改变，而后面的数据会用到前面的（这就可能遇上已经改变过的前面的数据），不正确了
+            for (int j = sum; j > 0; j--) {
+                if (j>=nums[i])
+                    dp[j] = dp[j] || dp[j-nums[i]];
+                else
+                    dp[j] = dp[j];
+            }
+        }
+        return dp[sum];
+    }
+}
--- src/main/java/ali_leetcode_daily/dynamic_programming/Solution518.java
+++ src/main/java/ali_leetcode_daily/dynamic_programming/Solution518.java
@@ -7,8 +7,6 @@ package ali_leetcode_daily.dynamic_programming;
  * @Date 2020/6/30 20:52
  **/
 
-import java.util.Arrays;
-
 /**
  * 给定不同面额的硬币和一个总金额。写出函数来计算可以凑成总金额的硬币组合数。
  * 输入: amount = 5, coins = [1, 2, 5]
@@ -20,11 +18,55 @@ import java.util.Arrays;
  * 5=1+1+1+1+1
  */
 public class Solution518 {
+    // 二维的
     public int change(int amount, int[] coins) {
-        if (amount < 0)
+        if (amount < 0 || coins == null)
             return 0;
 
-        Arrays.sort(coins);
+        // 当len==0&&amount==0的时候，结果是1，所以也走下面的逻辑
+        int len = coins.length;
+        int[][] dp = new int[len + 1][amount + 1];
 
+        // base case
+        for (int j = 1; j < amount + 1; j++) {
+            dp[0][j] = 0;
+        }
+        for (int i = 0; i < len + 1; i++) {
+            dp[i][0] = 1;
+        }
+
+        // dp
+        for (int i = 1; i < len + 1; i++) {
+            for (int j = 1; j < amount + 1; j++) {
+                if (j >= coins[i - 1]) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[len][amount];
+    }
+
+    // 压缩到一维
+    public int change2(int amount, int[] coins) {
+        if (amount < 0 || coins == null)
+            return 0;
+
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int i = 0; i < coins.length; i++) {
+            // 这需要j从前往后遍历
+            // 因为dp[j]的状态与[i-1]和[i]都有关，所以需要从前往后
+            for (int j = 1; j < amount + 1; j++) {
+                if (j - coins[i] >= 0)
+                    dp[j] += dp[j - coins[i]];
+                else
+                    dp[j] = dp[j];
+            }
+        }
+
+        return dp[amount];
     }
 }
