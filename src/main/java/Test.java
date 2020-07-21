cd4b23dccc9f20fd1de3e40c3dd6b8beb8f3309f
chiyue.sjx <chiyue.sjx@alibaba-inc.com>
2020-07-21 17:06:08 +0800

    Merge branch 'master' of /Users/chiyue/IdeaProjects/leetcode with conflicts.

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 /**
  * @ClassName Test
  * @Description TODO
@@ -8,8 +6,22 @@ import java.util.Arrays;
  **/
 public class Test {
     public static void main(String[] args) {
-        char[] s = {'e','a','t'};
-        Arrays.sort(s);
-        System.out.println(s);
+        String s = "www.taobao.com";
+        int i = s.length() - 1;
+
+        StringBuilder sb = new StringBuilder();
+
+        while (i >= 0) {
+            int j = i;
+            while (i >= 0 && s.charAt(i) != '.') {
+                i--;
+            }
+            // 当前i为'.'，所以需要从i+1开始截取字符串
+            sb.append(s, i + 1, j + 1);
+            if (i > 0)
+                sb.append('.');
+            i--;
+        }
+        System.out.println(sb.toString());
     }
 }
---
+++ src/main/java/Test2.java
@@ -0,0 +1,21 @@
+/**
+ * @Classname Test2
+ * @Description TODO
+ * @Date 2020/7/6 7:08 下午
+ * @Created by chiyue
+ */
+public class Test2 {
+    public static void main(String[] args) {
+//        HashMap<String, String> hashMap = new HashMap<>();
+//        hashMap.put("hi","1,2");
+//
+//        String hello = hashMap.get("hi");
+//
+//            if (hello!=null){
+//            String[] split = hello.split(",");
+//            System.out.println(split[0]);
+//        }
+
+        System.out.println(Math.ceil(120d/100d));
+    }
+}
